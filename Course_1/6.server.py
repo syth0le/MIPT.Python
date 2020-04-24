d41acbbb3fa5391e1aec839347182e89c864296e
syth0le <chdan565@gamil.com>
2020-04-25 02:06:18 +0300

    server WORKS

--- Course_1/6.server.py
+++ Course_1/6.server.py
@@ -1,7 +1,6 @@
 import asyncio
 
-data_storage = {}
-status = 'ok\n'
+data_storage = dict()
 
 
 class ClientServerProtocol(asyncio.Protocol):
@@ -12,65 +11,46 @@ class ClientServerProtocol(asyncio.Protocol):
         self.transport = transport
 
     def data_received(self, data: bytes):
-        resp = self.value_processing(data.decode())
+        decoding_data = data.decode()[:-1].split(" ")
+        resp = self.value_processing(decoding_data)
         self.transport.write(resp.encode())
 
-    def value_processing(self, string):
-        string_massive = string[:-1].split(" ")
-        if len(string_massive) != 1:
-            if string_massive[0] == "get":
-                return self.send_data(string_massive[1])
-            elif string_massive[0] == "put":
-                return self.receive_data(string_massive[1], string_massive[2], string_massive[3])
-            else:
-                return 'error\nwrong command\n\n'
+    def value_processing(self, param):
+        if param[0] == "put":
+            return self.receive_data(param[1], param[2], param[3])
+        elif param[0] == "get":
+            return self.send_data(param[1])
         else:
             return 'error\nwrong command\n\n'
 
     @staticmethod
     def send_data(key):
+        status = 'ok\n'
         if key == "*":
             for key, values in data_storage.items():
-                values.sort()
-                string = status
                 for items in values:
-                    try:
-                        string += f'{key} {items[1]} {items[0]}\n'
-                    except IndexError:
-                        return 'error\nwrong command\n\n'
+                    status += f'{key} {items[1]} {items[0]}\n'
         else:
             if key in data_storage:
-                string = status
-                massive = data_storage[key]
-                for val in massive:
-                    try:
-                        string += f"{key} {val[1]} {val[0]}\n"
-                    except IndexError:
-                        return 'error\nwrong command\n\n'
-            else:
-                string = status
-        return string + '\n'
+                for val in data_storage[key]:
+                    status += f"{key} {val[1]} {val[0]}\n"
+        return status + '\n'
 
     @staticmethod
     def receive_data(key, metric, timestamp):
-        local = [timestamp, metric]
-        if key not in data_storage:
+        status = 'ok\n'
+        local = (timestamp, metric)
+        if key not  in data_storage:
             data_storage[key] = list()
-        else:
-            for every in data_storage[key]:
-                if timestamp == every[0]:
-                    data_storage[key].remove(every)
-                    data_storage[key].append(local)
-                    data_storage[key].sort()
+        if local not  in data_storage[key]:
+            data_storage[key].append(local)
+            data_storage[key].sort()
         return status + '\n'
 
 
-def run_server(host='127.0.0.1', port=10001):
+def run_server(host, port):
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(
-        ClientServerProtocol,
-        host, port
-    )
+    coro = loop.create_server(ClientServerProtocol, host, port)
     server = loop.run_until_complete(coro)
 
     try:
@@ -83,5 +63,5 @@ def run_server(host='127.0.0.1', port=10001):
     loop.close()
 
 
-if __name__ == '__main__':
-    run_server()
+#if __name__ == '__main__':
+    #run_server('127.0.0.1', 10001)
