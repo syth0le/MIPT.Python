95a1226e17939d85e544aab0147e74495e57524d
syth0le <chdan565@gamil.com>
2020-04-26 17:38:01 +0300

    func upd

--- Course_2/1_factorize_test.py
+++ Course_2/1_factorize_test.py
@@ -1,6 +1,15 @@
 import unittest
 
 
+def factorize(x):
+    """
+    Factorize positive integer and return its factors.
+    :type x: int,>=0
+    :rtype: tuple[N],N>0
+    """
+    pass
+
+
 class TestFactorize(unittest.TestCase):
 
     def test_wrong_types_raise_exception(self):
