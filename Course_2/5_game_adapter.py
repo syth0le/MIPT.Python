51b69670aa7576a979aabaec231d9f3e3fac0be6
syth0le <chdan565@gamil.com>
2020-04-30 02:35:38 +0300

    end 3rd week

--- Course_2/5_game_adapter.py
+++ Course_2/5_game_adapter.py
@@ -1,3 +1,41 @@
+class System:
+    def __init__(self):
+        self.map = self.grid = [[0 for i in range(30)] for _ in range(20)]
+        self.map[5][7] = 1
+        self.map[5][2] = -1
+
+    def get_lightening(self, light_mapper):
+        self.lightmap = light_mapper.lighten(self.map)
+
+
+class Light:
+    def __init__(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+        self.lights = []
+        self.obstacles = []
+
+    def set_dim(self, dim):
+        self.dim = dim
+        self.grid = [[0 for i in range(dim[0])] for _ in range(dim[1])]
+
+    def set_lights(self, lights):
+        self.lights = lights
+        self.generate_lights()
+
+    def set_obstacles(self, obstacles):
+        self.obstacles = obstacles
+        self.generate_lights()
+
+    def generate_lights(self):
+        return self.grid.copy()
+
+
+system = System()
+lighter = Light()
+system.get_lightening(lighter)
+
+
 class MappingAdapter:
 
     def __init__(self, adaptee):
@@ -30,3 +68,5 @@ class MappingAdapter:
                 if massive[i][j] == num:
                     result.append((j, i))
         return result
+
+
---
+++ Course_2/6_game_observer.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+
+#class Engine:
+ #   pass
+
+
+class ObservableEngine(Engine):
+    def __init__(self):
+        self._subscribers = set()
+
+    def subscribe(self, subscriber):
+        self._subscribers.add(subscriber)
+
+    def unsubscribe(self, subscriber):
+        self._subscribers.remove(subscriber)
+
+    def notify(self, message):
+        for subscriber in self._subscribers:
+            subscriber.update(message)
+
+
+class AbstractObserver(ABC):
+    @abstractmethod
+    def update(self, message):
+        pass
+
+
+class ShortNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = set()
+
+    def update(self, message):
+        if message["title"] not in self.achievements:
+            self.achievements.add(message["title"])
+
+
+class FullNotificationPrinter(AbstractObserver):
+    def __init__(self):
+        self.achievements = []
+
+    def update(self, message):
+        if message not in self.achievements:
+            self.achievements.append(message)
+
